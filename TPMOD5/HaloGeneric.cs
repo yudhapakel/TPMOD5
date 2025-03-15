930f5e7f8a87fa212cd809b80f71d92d29b35152
yudhapakel <yudahp23@gmail.com>
2025-03-16 01:20:41 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ TPMOD5/HaloGeneric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPMOD5
+{
+    internal class HaloGeneric
+    {
+        public static void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+        public static void Main()
+        {
+            SapaUser("yudha");
+        }
+    }
+}
